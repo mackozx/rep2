65ec710f9c6777c99c379f40a978e911ac019152
macko2 <mackozx@gmail.com>
2018-10-15 21:37:23 +0200

    first commit

--- scratch/src/main/java/pt/trainings/maven/scratch/Greetings.java
+++ scratch/src/main/java/pt/trainings/maven/scratch/Greetings.java
@@ -4,7 +4,7 @@ public class Greetings {
 	private static final String GREETING = "Hello ";
 
 	public String sayHello(String string) {
-		return GREETING + (string != null ? string : "Jonh Doe");
+		return GREETING + (string != null ? string : "Jonh Doee");
 	}
 
 }
