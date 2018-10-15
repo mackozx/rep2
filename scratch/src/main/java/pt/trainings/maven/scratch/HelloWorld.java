a865e9f5481809af7c59814e636b8b17064dd293
macko2 <mackozx@gmail.com>
2018-10-15 18:17:53 +0200

    first commit

---
+++ scratch/src/main/java/pt/trainings/maven/scratch/HelloWorld.java
@@ -0,0 +1,10 @@
+package pt.trainings.maven.scratch;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Greetings greetings = new Greetings();
+		greetings.sayHello(args[0]);
+	}
+
+}
